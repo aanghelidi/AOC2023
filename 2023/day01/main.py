260c95c8258ec707e25e5d6d52c86c6cbb68329d
Alain Anghelidi <alainanghelidi@gmail.com>
2023-12-01 12:54:08 +0100

    feat: fix regex solution

--- 2023/day01/main.py
+++ 2023/day01/main.py
@@ -1,17 +1,18 @@
+import re
+
 with open("input.txt") as f:
     data = f.read().splitlines()
 numsd = dict(zip("one two three four five six seven eight nine".split(), "123456789"))
 ans, ans2 = 0, 0
+numbers = re.compile(r"\d{1}")
+numbers_or_str = re.compile(r"(?=(\d{1}|one|two|three|four|five|six|seven|eight|nine))")
 for line in data:
-    nums, nums2 = [], []
-    for i, c in enumerate(line):
-        if c.isdigit():
-            nums.append(c)
-            nums2.append(c)
-        for k, v in numsd.items():
-            if line[i:].startswith(k):
-                nums2.append(v)
-    ans += int(nums[0] + nums[-1])
-    ans2 += int(nums2[0] + nums2[-1])
+    nums = numbers.findall(line)
+    joined = nums[0] + nums[-1]
+    ans += int(joined)
+    # Part 2
+    nums = [n for n in numbers_or_str.findall(line) if n != ""]  # ugly
+    joined = numsd.get(nums[0], nums[0]) + numsd.get(nums[-1], nums[-1])
+    ans2 += int(joined)
 print(f"Part 1: {ans}")
 print(f"Part 2: {ans2}")
