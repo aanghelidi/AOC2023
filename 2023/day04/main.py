0f2d76c9ea0f22b26b44e0f09fcc1e052373d8ee
Alain Anghelidi <alainanghelidi@gmail.com>
2023-12-04 20:43:33 +0100

    enh: improve performance with cache, generator and set

--- 2023/day04/main.py
+++ 2023/day04/main.py
@@ -1,6 +1,8 @@
 import sys
+from collections import defaultdict
 from dataclasses import dataclass, field
-from typing import DefaultDict
+from functools import lru_cache
+from typing import Generator
 
 with open(sys.argv[1]) as f:
     data = f.read().splitlines()
@@ -9,33 +11,33 @@ with open(sys.argv[1]) as f:
 @dataclass
 class Card:
     id: int
-    winning_numbers: list[int] = field(default_factory=list)
-    numbers: list[int] = field(default_factory=list)
+    winning_numbers: set[int] = field(default_factory=set)
+    numbers: set[int] = field(default_factory=set)
 
     @classmethod
     def parse(cls, data, id):
-        all_numbers = data.split(": ")[1]
-        winning_numbers, numbers = all_numbers.split(" | ")
-        winning_numbers = [int(x) for x in winning_numbers.split()]
-        numbers = [int(x) for x in numbers.split()]
+        winning_numbers, numbers = data.split(": ")[1].split(" | ")
+        winning_numbers = {int(x) for x in winning_numbers.split()}
+        numbers = {int(x) for x in numbers.split()}
         return cls(id, winning_numbers, numbers)
 
-    def matching_winning_numbers(self) -> list[int]:
-        return [x for x in self.numbers if x in self.winning_numbers]
+    def matching_winning_numbers(self) -> set[int]:
+        return self.winning_numbers & self.numbers
 
-    def copies(self, n_matches: int, current_id: int) -> list["Card"]:
-        return [Card(current_id + i) for i in range(1, n_matches + 1)]
+
+@lru_cache
+def retrieve_copies(n_matches: int, current_id: int) -> Generator[Card, None, None]:
+    return (Card(current_id + i) for i in range(1, n_matches + 1))
 
 
 ans = 0
-card_counter = DefaultDict(int)
+card_counter = defaultdict(int)
 for i, line in enumerate(data, start=1):
     card = Card.parse(line, i)
     card_counter[card.id] += 1
     matching_winning_numbers = card.matching_winning_numbers()
     for _ in range(card_counter[card.id]):
-        copies = card.copies(len(matching_winning_numbers), i)
-        for copy in copies:
+        for copy in retrieve_copies(len(matching_winning_numbers), i):
             card_counter[copy.id] += 1
     if len(matching_winning_numbers) > 0:
         ans += 2 ** (len(matching_winning_numbers) - 1)
